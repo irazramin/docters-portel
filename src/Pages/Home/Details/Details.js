b007088c597df00de835875f87c694f7b31d864e
irazramin <irazramin@gmail.com>
2022-05-10 16:11:05 +0600

    details section added

--- src/Pages/Home/Details/Details.js
+++ src/Pages/Home/Details/Details.js
@@ -1,22 +1,25 @@
-import React from 'react'
-
+import React from 'react';
+import treatment from "../../../assets/images/treatment.png";
 const Details = () => {
   return (
-    <div class='hero min-h-screen bg-base-200'>
-      <div class='hero-content flex-col lg:flex-row'>
-        <img
-          src='https://api.lorem.space/image/movie?w=260&h=400'
-          class='max-w-sm rounded-lg shadow-2xl'
-          alt=''
-        />
-        <div>
-          <h1 class='text-5xl font-bold'>Box Office News!</h1>
+    <div class='hero mt-[154px] mx-[26px]'>
+      <div class='hero-content flex-col lg:flex-row justify-evenly '>
+        <img src={treatment} class='max-w-sm rounded-lg shadow-2xl' alt='' />
+        <div className='lg:ml-[100px]'>
+          <h1 class='text-5xl font-bold'>
+            Exceptional Dental Care, on Your Terms
+          </h1>
           <p class='py-6'>
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
+            It is a long established fact that a reader will be distracted by
+            the readable content of a page when looking at its layout. The point
+            of using Lorem Ipsumis that it has a more-or-less normal
+            distribution of letters,as opposed to using 'Content here, content
+            here', making it look like readable English. Many desktop publishing
+            packages and web page
           </p>
-          <button class='btn btn-primary'>Get Started</button>
+          <button class='btn btn-primary text-white uppercase font-semibold'>
+            Get Started
+          </button>
         </div>
       </div>
     </div>
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,14 +1,18 @@
 import React from 'react'
+import Appointment from '../Appointment/Appointment'
 import Banner from './Banner/Banner'
+import Details from './Details/Details'
 import Info from './Info/Info'
 import Services from './Services/Services'
 
 const Home = () => {
   return (
-    <div>
+    <div className=''>
       <Banner />
       <Info />
       <Services />
+      <Details />
+      <Appointment />
     </div>
   )
 }
